22077473b02b892d5c510f3719a1d27e60f54978
Horacio Caniz <horacio.caniz@galileo.edu>
2021-02-08 22:26:23 -0600

    first commit

---
+++ Actividad2_21000764.js
@@ -0,0 +1,24 @@
+var mes=10
+var mes=prompt("Ingrese número del mes a verificar")
+
+
+if(mes==12||mes==1||mes==2){
+  
+  alert("Es invierno")
+  
+}else if (mes==3||mes==4||mes==5){
+  
+  alert("Es primavera")
+  
+}else if(mes==6||mes==7||mes==8){
+  
+  alert("Es verano")
+  
+}else if(mes==9||mes==10||mes==11){
+  
+  alert("Es otoño")
+}else{
+  alert("Ingrese número del 1 al 12")
+}
+
+console.log("carnet= 21000764 Horacio Manuel Caniz Argueta")
